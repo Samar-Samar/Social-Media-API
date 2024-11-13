fddb3c725ba190af955bd5aba419c06e8a815a72
Samar-Samar <anmolbhatiajee@gmail.com>
2024-11-13 21:40:08 +0530

    updates

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from .database import create_db_and_tables
 from fastapi import FastAPI
 from .routers import posts, users, auth, votes
+from fastapi.middleware.cors import CORSMiddleware
 
 create_db_and_tables()
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
